09dea5e2dccc372333f621fe364920ceac4f6dc0
donskilful <donskilful@gmail.com>
2022-07-10 15:45:16 +0100

    Searvh bar adjusted

--- src/components/TopSection/index.jsx
+++ src/components/TopSection/index.jsx
@@ -6,20 +6,24 @@ import { ReactComponent as FilterIcon } from 'assets/svg/Filter.svg';
 const TopSection = () => {
   return (
     <section className="top-section section">
-      <div className="search-bar">
-        <div className="search-left">
-          <input type="text" className="search-box" placeholder="Search a book, Author or review, we would find it" />
-          <div className="search-icon">
-            <SearchIcon />
+      <form action="" method="post">
+        <div className="search-bar">
+          <div className="search-left">
+            <input type="text" className="search-box" placeholder="Search a book, Author or review, we would find it" />
+            <div className="search-icon">
+              <a href="#">
+                <SearchIcon />
+              </a>
+            </div>
+          </div>
+          <div className="search-right">
+            <div className="filter-icon">
+              <FilterIcon />
+            </div>
+            <p className="filter-text">Filter</p>
           </div>
         </div>
-        <div className="search-right">
-          <div className="filter-icon">
-            <FilterIcon />
-          </div>
-          <p className="filter-text">Filter</p>
-        </div>
-      </div>
+      </form>
       <div className="heading">
         <h2 className="great-stories">Great Stories</h2>
         <h1 className="our-passion">are our passion .</h1>
