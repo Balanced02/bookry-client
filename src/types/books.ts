46c22872e89579de725eb0dfa0d8c1bdee932efc
donskilful <donskilful@gmail.com>
2022-07-17 21:45:01 +0100

    housekeeping: fixed styling and added icon to section heading

--- src/types/books.ts
+++ src/types/books.ts
@@ -1,8 +1,10 @@
+import { ReactNode } from 'react';
+
 export type BookT = { id?: number; img: string; title: string; rating: string };
 
 export type BookCarouselT = {
   title: string;
-  icon: string;
-  description: string;
-  data: BookT[];
+  icon?: ReactNode;
+  description: ReactNode;
+  books: BookT[];
 };
