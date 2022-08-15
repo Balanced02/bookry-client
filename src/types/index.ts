ba1e5b92d7dac611f093bccecb7c8b515196f3c4
Adepoju Daniel <adebalanced02@gmail.com>
2022-08-15 21:24:07 +0100

    :bug: fix merge conflict

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,5 @@
 export * from './books';
 export * from './user';
-<<<<<<< HEAD
 export * from './member';
-=======
 export * from './button';
 export * from './input';
->>>>>>> 62bbc6a7059bba2273c52f5548e0fa0d7a6acd58
